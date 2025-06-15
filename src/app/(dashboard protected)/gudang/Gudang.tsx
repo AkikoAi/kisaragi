53307852661474f3f086b23e295f1e0224a77112
AkikoAI <hadifirmansya645@gmail.com>
2025-06-15 18:09:55 +0700

    Input template

--- src/app/(dashboard protected)/gudang/Gudang.tsx
+++ src/app/(dashboard protected)/gudang/Gudang.tsx
@@ -9,19 +9,28 @@ export default function Gudang() {
         if (isRefreshing) return;
         setRefreshing(true);
         console.log("Refreshing list");
-        setRefreshing(false);
+        setTimeout(() => {
+            setRefreshing(false);
+        }, 5000);
     }//
 
     return (<>
-        {/* Grid Card Section */}
-        <div className="mt-10 flex justify-end">
-            <button
-                className="flex gap-2 items-center"
-                onClick={refreshList}>
+        <div className="fixed w-60 h-60 bg-gray-700/97 rounded-md">
+        Ip
+        </div>
+        <div className="mt-10 flex justify-between">
+            <button className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition-colors
+            cursor-pointer">
+                Baru
+            </button>
+            <button className="flex gap-2 items-center px-4 py-2 rounded-md border border-gray-300 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer"
+                onClick={refreshList}
+                disabled={!!isRefreshing}>
                 <RiRefreshLine className={isRefreshing ? "animate-spin" : ""} />
                 <span>Segarkan</span>
             </button>
         </div>
+        {/* Grid Card Section */}
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-7">
 
             <div
