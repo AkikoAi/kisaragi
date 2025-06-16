0e1d536ddeb1d63b98c407bca992ef16f3499afa
AkikoAI <hadifirmansya645@gmail.com>
2025-06-16 16:32:39 +0700

    Update frontend, menambahkan authentikasi dan mengirim data user ke navigation

--- src/app/(dashboard protected)/layout.tsx
+++ src/app/(dashboard protected)/layout.tsx
@@ -4,20 +4,28 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import Navigation from "./(Layout)/Navigation";
+import { verifyTokenJWT } from "@/utils/ksr_jwt";
 
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-    /*
-    const cookie = await cookies();
-    const auth = cookie.get("Auth")?.value;
-    if (!auth) return redirect("/login");
-    */
-    return (<>
-        
-            <Navigation />
-            <main className="flex-1 container mx-auto px-4 py-6">
-                {children}
-            </main>
+    try {
 
-    </>)
+        const cookie = await cookies();
+        const auth = cookie.get("Auth")?.value;
+        if (!auth) return redirect("/login");
+        const data = verifyTokenJWT(auth);
+
+        return (<>
+
+            <div className="min-h-screen bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-100 flex flex-col">
+                <Navigation data={data} />
+                <main className="flex-1 container mx-auto px-4 py-6">
+                    {children}
+                </main>
+            </div>
+
+        </>)
+    } catch (e) {
+        return redirect("/login");
+    }
 }
