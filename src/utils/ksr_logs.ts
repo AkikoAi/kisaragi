15796dbc12f0e9634d55629d273d5d5d1bbeddb1
AkikoAI <hadifirmansya645@gmail.com>
2025-06-16 19:21:29 +0700

    Menambahkan fungsi logs

--- src/utils/ksr_logs.ts
+++ src/utils/ksr_logs.ts
@@ -1,12 +1,22 @@
 import { appendFile } from "fs";
 import path from "path";
 
+
+
+export function addLogsUser(logMessage: string) {
+    return appendFile("src/logs/user.txt", `[${new Date().toISOString()}] : ${logMessage}\n`, (err) => {
+        if (err) console.error('Error writing to log file:', err);
+    });
+}
+
 export function addLogs(logMessage: string) {
     return appendFile("src/logs/log.txt", `[${new Date().toISOString()}] : ${logMessage}\n`, (err) => {
         if (err) console.error('Error writing to log file:', err);
     });
 }
 
-export function addLogsFE(error:unknown){
-    return addLogs((error as Error).message);
+export function addLogsFE(error: unknown) {
+    return appendFile("src/logs/error.txt", `[${new Date().toISOString()}] : ${(error as Error).message}\n`, (err) => {
+        if (err) console.error('Error writing to log file:', err);
+    });
 }
