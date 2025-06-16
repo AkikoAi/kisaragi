8dafa05141d2ed43bd097f7ba64c42ee180f99f9
AkikoAI <hadifirmansya645@gmail.com>
2025-06-16 16:34:53 +0700

    Menambahkan backend, Fungsi GET menampilkan informasi server.

---
+++ src/app/api/(protected)/(Level 90)/server-information/route.ts
@@ -0,0 +1,43 @@
+import { verifyTokenJWT } from '@/utils/ksr_jwt';
+import { addLogsFE } from '@/utils/ksr_logs';
+import ksr_status from '@/utils/ksr_status';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
+import os from 'os';
+
+export async function GET() {
+    try {
+        const cookie = await cookies();
+        const token = cookie.get("Auth")?.value as string;
+        const data = verifyTokenJWT(token);
+
+        if (data.privilege < 90) return NextResponse.json({ status: false, msg: ksr_status.unauthorized });
+        try {
+            const cpuInfo = os.cpus();
+            const memoryInfo = {
+                total: os.totalmem(),
+                free: os.freemem()
+            };
+            const uptime = os.uptime();
+
+            return NextResponse.json({
+                status: true, data: {
+                    platform: os.platform(),
+                    arch: os.arch(),
+                    cpuCount: cpuInfo.length,
+                    cpuModel: cpuInfo[0].model,
+                    memoryInfo,
+                    uptimeSeconds: uptime
+                }
+            });
+        } catch (e) {
+            addLogsFE(e);
+            return NextResponse.json({ status: false, msg: ksr_status[500] });
+        }
+
+
+    } catch (e) {
+        return redirect("/login");
+    }
+}
