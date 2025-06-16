99a4af6de22a41af061802460b86c87e8ef5f083
AkikoAI <hadifirmansya645@gmail.com>
2025-06-16 23:31:09 +0700

    update frontend, menambahkan page management users

---
+++ src/app/(frontend)/(dashboard protected)/(61-ManagerOrAdmin)/management-users/page.tsx
@@ -0,0 +1,31 @@
+"use server";
+import Unauthorized from "@/app/(frontend)/(unprotected)/(error)/unauthorized/Unauthorized";
+import { verifyTokenJWT } from "@/utils/ksr_jwt";
+import { Metadata } from "next"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+/**
+export const metadata: Metadata = {
+    title: "User Management"
+}
+ */
+
+export default async function Page() {
+    try {
+        const cookie = await cookies();
+        const token = cookie.get("Auth")?.value as string;
+        const data = verifyTokenJWT(token);
+        if (data.privilege < 61) return <div className="mt-10"><Unauthorized />
+        </div>
+
+        return (<>
+            Hello world
+        </>)
+
+    } catch (e) {
+        console.log(e);
+        return redirect("/login");
+    }
+
+}
