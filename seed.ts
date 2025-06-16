8ec4e3b8fe09e32406698ba15ea66582fce2613f
AkikoAI <hadifirmansya645@gmail.com>
2025-06-16 18:35:56 +0700

    Memperbarui seed, menambahkan user,moderator,admin

--- seed.ts
+++ seed.ts
@@ -5,7 +5,7 @@ async function seed() {
     try {
         console.info("menambahkan seed")
         const pass = await hashPassword(process.env.ADMIN_PASSWORD || "12345678");
-        const ADMIN = await prisma.user.upsert({
+        const SUPERADMIN = await prisma.user.upsert({
             where: {
                 username: process.env.ADMIN_USERNAME || "AkikoAi"
             },
@@ -16,9 +16,57 @@ async function seed() {
                 username: process.env.ADMIN_USERNAME || "AkikoAi",
                 password: pass,
                 name: process.env.ADMIN_NAME || "Hadi Firmansya",
-                email: process.env.ADMIN_EMAIL || "akiko@kisaragi.id",
+                email: process.env.ADMIN_EMAIL || "superadmin.akiko@kisaragi.id",
                 privilege: 100,
                 role: "superadmin"
+            }
+        });
+        const ADMIN = await prisma.user.upsert({
+            where: {
+                username: "AkikoAi-Admin"
+            },
+            update: {
+                password: pass
+            },
+            create: {
+                username: "AkikoAi-Admin",
+                password: pass,
+                name: "Hadi Firmansya - Admin",
+                email: "admin.akiko@kisaragi.id",
+                privilege: 90,
+                role: "admin"
+            }
+        });
+        const MODERATOR = await prisma.user.upsert({
+            where: {
+                username: "AkikoAi-Moderator"
+            },
+            update: {
+                password: pass
+            },
+            create: {
+                username: "AkikoAi-Moderator",
+                password: pass,
+                name: "Hadi Firmansya - Moderator",
+                email: "moderator.akiko@kisaragi.id",
+                privilege: 60,
+                role: "moderator"
+            }
+        });
+        const USER = await prisma.user.upsert({
+            where: {
+                username: "AkikoAi-User"
+            },
+            update: {
+                password: pass
+            },
+            create: {
+                username: "AkikoAi-User",
+                password: pass,
+                name: "Hadi Firmansya - User",
+                email: "user.akiko@kisaragi.id",
+                privilege: 30,
+                role: "user"
             }
         });
         console.info("Selesai melakukan seeder", ADMIN);
