221a077bd1dde44ad23c7d6209085cf6776fc76b
AkikoAi <hadifirmansya644@gmail.com>
2025-06-18 20:35:55 +0700

    update frontend, memperbaiki tampilan navigation dan mengubah tampilannya secara penuh

---
+++ src/app/(frontend)/Components/Navigation.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { verifyToken } from "@/utils/ksr_jwt";
+import Image from "next/image";
+import Link from "next/link";
+import { Fragment, InputEvent, InputHTMLAttributes, ReactElement, useState } from "react";
+import { BiLogOut, BiX } from "react-icons/bi";
+import { CgEnter } from "react-icons/cg";
+import { FaBars, FaCog, FaUserTie } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+
+import { GiFox, GiFoxTail } from "react-icons/gi";
+import { MdWarning } from "react-icons/md";
+
+type menuType = {
+    name: string;
+    path: string | null;
+    prefetch: boolean;
+}
+
+const menu: menuType[] = [
+    {
+        name: "Management User",
+        path: "/management-users",
+        prefetch: true
+    },
+    {
+        name: "Dashboard",
+        path: "/dashboard",
+        prefetch: true
+    },
+    {
+        name: "Logout",
+        path: "/api/logout",
+        prefetch: false
+    }
+]
+
+
+function ProfilePicture({ data }: { data: verifyToken }) {
+    return <div className="relative flex items-center justify-center rounded-full bg-gray-200 dark:bg-gray-700 overflow-hidden">
+        {data.avatarUrl ? (
+            <Image
+                width={400}
+                height={400}
+                src={data.avatarUrl}
+                alt={`Foto profil ${data.name}`}
+                className="w-10 h-10 rounded-full"
+            />
+        ) : (
+            <GiFox className="w-10 h-10 text-orange-500" />
+        )}
+    </div>
+}
+
+export default function Navigation({ data }: { data: verifyToken }) {
+    const [filteredMenu, setFilteredMenu] = useState<menuType[]>(menu);
+
+    function searchAction(searchInput: string) {
+        if (!searchInput) {
+            setFilteredMenu(menu);
+            return
+        };
+        const filteringResult = menu.filter(({ name }) => {
+            if (name.toLowerCase().includes(searchInput.toLowerCase())) return true;
+            return false
+        });
+        setFilteredMenu(filteringResult);
+    }
+
+    function logoutAction() {
+        if (confirm("Konfirmasi ingin keluar?")) {
+            alert("Semoga harimu menyenangkan!");
+            return window.location.href = "/api/logout";
+        }
+        alert("Selamat bekerja kembali!");
+    }
+
+    return (
+        <nav className="w-full bg-gray-100 dark:bg-gray-800 shadow-md p-4">
+            {/* Universal Navigation */}
+            <div className="flex justify-between items-center h-8">
+                <div>
+                    <label
+                        htmlFor="menuViewModals"
+                        className="cursor-pointer">
+                        <FaBars />
+                    </label>
+                </div>
+                <div>
+                    <label
+                        htmlFor="profileDropdown"
+                        className="cursor-pointer">
+                        <ProfilePicture data={data} />
+                    </label>
+                </div>
+            </div>
+            <div>
+                <input type="checkbox" id="menuViewModals" className="hidden peer" />
+                <div className="hidden peer-checked:block fixed inset-0 bg-black/90 z-50">
+                    <div className="flex flex-col p-4">
+                        <label htmlFor="menuViewModals"
+                            className="cursor-pointer text-3xl">
+                            <BiX />
+                        </label>
+                        <div className="mx-auto mt-4 not-md:max-w-[99%] w-[37rem] flex items-center justify-center bg-white dark:bg-gray-800 rounded-xl">
+                            <div className="w-full max-w-[37rem] max-h-[90%] bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md space-y-4">
+                                {/* Search Menu */}
+                                <div>
+                                    <div className="relative flex items-center">
+                                        <button className="absolute right-3 inset-y-0 text-gray-400"><FiSearch /></button>
+                                        <input type="text" placeholder="Cari ..." className="w-full pr-8 px-3 py-2 border rounded-md bg-gray-50 dark:bg-gray-700"
+                                            onChange={(e) => searchAction(e.currentTarget.value)} />
+                                    </div>
+                                </div>
+                                <div className="mt-2 bg-white dark:bg-gray-700 border border-gray-200 dark:border-gray-600 dark:text-gray-200">
+                                    <ul className="p-2">
+                                        {
+                                            filteredMenu.length > 0 ? filteredMenu.map(({ name, path, prefetch }, index, arr) =>
+                                                <Fragment key={index}>
+                                                    {prefetch ? <Link href={path || "#"} >
+                                                        <li className="flex items-center gap-2 my-2 py-3 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
+                                                            <GiFoxTail />
+                                                            {name}
+                                                        </li>
+                                                    </Link>
+                                                        : <a href={path || "#"}>
+                                                            <li className="flex items-center gap-2 my-2 py-3 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
+                                                                <GiFoxTail />
+                                                                {name}
+                                                            </li>
+                                                        </a>
+                                                    }
+                                                    {index + 1 != arr.length && <hr />}
+                                                </Fragment>
+                                            ) :
+                                                <li className="flex items-center gap-2 py-3 px-2">
+                                                    <MdWarning />
+                                                    Tidak ada hasil yang ditemukan
+                                                </li>
+                                        }
+                                    </ul >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div>
+                <input type="checkbox" id="profileDropdown" className="hidden peer" />
+                <div className="hidden peer-checked:block absolute right-0 mt-2 w-40 bg-white dark:bg-gray-700 border border-gray-200 dark:border-gray-600 rounded-md shadow-lg z-40 dark:text-gray-200">
+                    <ul className="p-2">
+                        <li className="flex items-center gap-2 py-1 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
+                            <FaUserTie />
+                            Profile</li>
+                        <hr />
+                        <li className="flex items-center gap-2 py-1 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
+                            <FaCog />Setting</li>
+                        <hr />
+                        <li className="flex items-center gap-2 py-1 px-2 text-red-400 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer"
+                            onClick={logoutAction}
+                        >
+                            <BiLogOut />Logout
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Desktop Navigation */}
+
+            {/* Mobile Navigation */}
+
+
+            {/* Mobile Menu Dropdown */}
+
+        </nav >
+    );
+}
