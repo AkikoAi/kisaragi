760b64c99311810b3624f228583ffc5e695b6c6d
AkikoAI <hadifirmansya645@gmail.com>
2025-06-18 15:38:20 +0700

    Update config agar support gambar

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      hostname: "encrypted-tbn0.gstatic.com",
+      protocol: "https",
+      pathname: "/**"
+    }]
+  }
 };
 
 export default nextConfig;
