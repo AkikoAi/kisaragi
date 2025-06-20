85551477139c848aab74073f6de0a55221d3de64
AkikoAi <hadifirmansya644@gmail.com>
2025-06-20 19:40:54 +0700

    update backend, menambahkan handling error, jika input unique double

--- src/app/api/(protected)/profile/route.ts
+++ src/app/api/(protected)/profile/route.ts
@@ -1,9 +1,12 @@
+import { Prisma } from "@/generated/prisma";
 import DataAccessLayer from "@/utils/DataAccessLayer";
 import prisma from "@/utils/db";
 import { addLogsFE } from "@/utils/ksr_logs";
 import ksr_status from "@/utils/ksr_status";
 import { profilePost } from "@/utils/validation";
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/react-native.js"
+
 
 export async function POST(req: NextRequest) {
     try {
@@ -13,7 +16,6 @@ export async function POST(req: NextRequest) {
         const { email, name, avatarUrl } = await req.json();
         const validationResult = profilePost.safeParse({ email, name, avatarUrl });
         if (!validationResult.success) return NextResponse.json({ status: false, msg: JSON.parse(validationResult.error.message) });
-
         // Mulai mengubah data
         const updateResult = await prisma.user.update({
             where: { id: data.id },
@@ -23,9 +25,15 @@ export async function POST(req: NextRequest) {
                 avatarUrl: validationResult.data.avatarUrl
             }
         });
-
         return NextResponse.json({ status: true, data: "Profile berhasil diubah" });
     } catch (error) {
+        const err = error as Prisma.PrismaClientKnownRequestError;
+        if (err.code === 'P2002') {
+            return NextResponse.json({
+                status: false,
+                msg: `Ups! Ada data kembar nih Fubuki-chan! 😱 ${err.meta?.target} sudah digunakan!`,
+            });
+        }
         addLogsFE(error);
         return NextResponse.json({ status: false, msg: ksr_status[500] });
     }
